7971846462b4460846ae992ef5d3ebe7795331ed
firdaauliar <fauliarahma05@gmail.com>
2025-01-24 16:43:10 +0700

    configuration to deploy to netlify

--- part3/phonebook-backend/src/api.js
+++ part3/phonebook-backend/src/api.js
@@ -2,8 +2,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const serverless = require('serverless-http')
 
 const app = express()
+const router = express.Router()
 
 app.use(cors())
 // Or, allow only a specific origin like your frontend (http://localhost:5173):
@@ -44,18 +46,18 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :b
 // morgan("tiny", {stream: (request, res)=>{return res.statusCode}})
 // morgan(':method :url :status :res[content-length] - :response-time ms')
 
-app.get('/',(request, response)=>response.send("<h1>Hello World</h1>"))
+router.get('/',(request, response)=>response.send("<h1>Hello World</h1>"))
 
-app.get('/api/persons', (request, response)=>{
+router.get('/persons', (request, response)=>{
     response.json(persons)
 })
 
-app.get('/info', (request, response)=>{
+router.get('/info', (request, response)=>{
     let currentTime = new Date()
     response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${currentTime}</p>`)
 })
 
-app.get('/api/persons/:id',(request, response)=>{
+router.get('/persons/:id',(request, response)=>{
     const personsId = request.params.id
     const person = persons.find(person=>person.id === personsId)
     if(person){
@@ -65,14 +67,14 @@ app.get('/api/persons/:id',(request, response)=>{
     }
 })
 
-app.delete('/api/persons/:id', (request, response)=>{
+router.delete('/persons/:id', (request, response)=>{
     const id = request.params.id
     persons = persons.filter(person=>person.id !== id)
 
     response.status(204).end()
 })
 
-app.post('/api/persons', (request, response)=>{
+router.post('/persons', (request, response)=>{
     const body = request.body
     if(!body.name || !body.number){
         return response.status(400).json({
@@ -94,12 +96,15 @@ app.post('/api/persons', (request, response)=>{
     response.json(person)
 })
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
+// const unknownEndpoint = (request, response) => {
+//     response.status(404).send({ error: 'unknown endpoint' })
+//   }
   
-app.use(unknownEndpoint)
+// app.use(unknownEndpoint)
 
-const PORT = 3001
+app.use('/.netlify/functions/api', router)
+module.exports.handler = serverless(app)
 
-app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
+// const PORT = 3001
+
+// app.listen(PORT, ()=>console.log(`server running on port ${PORT}`))
